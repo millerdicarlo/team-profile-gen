399115b23b01b7d461bc2b6e45427defe1484067
millerd <millerdicarlo@gmail.com>
2022-09-07 20:12:35 -0400

    generateHTML.js and its code written

--- index.js
+++ index.js
@@ -9,3 +9,26 @@ const Intern = require('./lib/Intern');
 // node modules
 const fs = require('fs'); 
 const inquirer = require('inquirer');
+
+// team array
+const teamArray = [];
+
+
+// start of manager prompts 
+
+
+// function to generate HTML page file using file system 
+
+
+
+addManager()
+  .then(addEmployee)
+  .then(teamArray => {
+    return generateHTML(teamArray);
+  })
+  .then(pageHTML => {
+    return writeFile(pageHTML);
+  })
+  .catch(err => {
+ console.log(err);
+  });
