588050ad2145679c8c3e858b0b5e7402fc13f52f
millerd <millerdicarlo@gmail.com>
2022-09-07 20:34:42 -0400

    HTML generator function added

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
-console.log('Node');
+//console.log('Hello Node');
 
-/*
 //link page creation
 const generateHTML = require('./src/generateHTML');
 
@@ -21,7 +20,18 @@ const teamArray = [];
 
 
 // function to generate HTML page file using file system 
-
+const writeFile = data => {
+    fs.writeFile('./dist/index.html', data, err => {
+        // if there is an error 
+        if (err) {
+            console.log(err);
+            return;
+        // when the profile has been created 
+        } else {
+            console.log("Your team profile has been successfully created! Please check out the index.html")
+        }
+    })
+}; 
 
 
 addManager()
@@ -35,4 +45,4 @@ addManager()
   .catch(err => {
  console.log(err);
   });
-  */
+  
